eec1892bba50df4cdd96d9914599b53ee5679923
halilkrkn <halilkrkn000@gmail.com>
2024-05-18 00:01:57 +0300

    Has Been Created Navigation Integration and Bottom Bar Navigation

---
+++ app/src/main/java/com/halilkrkn/chatchef/navigation/util/BottomBarNavigationScreen.kt
@@ -0,0 +1,23 @@
+package com.halilkrkn.chatchef.navigation.util
+
+import androidx.annotation.DrawableRes
+import com.halilkrkn.chatchef.R
+
+sealed class BottomBarNavigationScreen(
+    val route: String,
+    val title: String,
+    @DrawableRes
+    val icon: Int,
+) {
+    data object ChatGptBottomBarNavigation : BottomBarNavigationScreen(
+        route = "chat_gpt",
+        title = "Chat GPT",
+        icon = R.drawable.chat_gpt_icon
+    )
+
+    data object FavoriteBottomBarNavigation : BottomBarNavigationScreen(
+        route = "profile",
+        title = "Profile",
+        icon = R.drawable.favorite_icon
+    )
+}
