212492c94641d0f250c5e18db7dcc98a871e9d43
Berkay Kurtoğlu <berkayberkay300@gmail.com>
2024-05-17 23:39:47 +0300

    Ai Chat Message Bubble is added.

--- app/src/main/java/com/halilkrkn/chatchef/presentation/components/AIChatMessage.kt
+++ app/src/main/java/com/halilkrkn/chatchef/presentation/components/AIChatMessage.kt
@@ -1,20 +1,82 @@
 package com.halilkrkn.chatchef.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.halilkrkn.chatchef.ui.theme.textPink
 
 @Composable
 fun AiChatMessage(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    message : String = "",
+    fontWeight: FontWeight = FontWeight.SemiBold,
+    fontSize : TextUnit = 13.sp,
+    fontFamily: FontFamily = FontFamily.Default,
+    loading : @Composable () -> Unit = { }
 ) {
 
-
-
+    Card(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(0.75f),
+        shape = RoundedCornerShape(
+            topStart = 0.dp,
+            topEnd = 15.dp,
+            bottomEnd = 15.dp,
+            bottomStart = 15.dp
+        ),
+    ) {
+        Column(
+            modifier = modifier
+                .padding(15.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                buildAnnotatedString {
+                    append("Hello! ")
+                    withStyle(
+                        style = SpanStyle(
+                            color = textPink
+                        )
+                    ){
+                        append("Emmanuel")
+                    }
+                    append(", I am ChatChef's AI. How can I help you?")
+                },
+                fontWeight = fontWeight,
+                fontSize = fontSize,
+                fontFamily = fontFamily,
+                textAlign = TextAlign.Start,
+                lineHeight = 20.sp
+            )
+            //loading()
+        }
+    }
 }
 
 
-@Preview
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 private fun AiChatMessagePreview() {
     AiChatMessage()
