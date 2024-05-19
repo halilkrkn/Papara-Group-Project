c2ec01d34cb883ce6aaae080ff7df4db5471f904
Busra Oner <sukriyebusraoner@gmail.com>
2024-05-19 12:57:46 +0300

    LoginScreen.kt added

---
+++ app/src/main/java/com/halilkrkn/chatchef/presentation/LoginPage/LoginScreen.kt
@@ -0,0 +1,16 @@
+package com.halilkrkn.chatchef.presentation.LoginPage
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun LoginScreenPreview() {
+
+}
