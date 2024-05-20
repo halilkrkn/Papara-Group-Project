6717ec6a1a512ef96b8b6bf41b049525009d6979
Enes Yıldırım <104741427+yildirimenes@users.noreply.github.com>
2024-05-20 11:11:07 +0300

    Merge pull request #12
    * add openai api
    
    * update model remote api

---
+++ app/src/main/java/com/halilkrkn/chatchef/ChatChefApplication.kt
@@ -0,0 +1,7 @@
+package com.halilkrkn.chatchef
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChatChefApplication : Application()
---
+++ app/src/main/java/com/halilkrkn/chatchef/data/remote/dto/MessageResponse.kt
@@ -0,0 +1,5 @@
+package com.halilkrkn.chatchef.data.remote.dto
+
+data class MessageResponse(
+    val message: Message
+)
