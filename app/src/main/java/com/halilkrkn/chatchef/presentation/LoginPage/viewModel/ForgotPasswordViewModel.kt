cd7e3780f8ce1a3ec065008b98d8862e098f175c
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-05-21 03:30:43 +0300

    Forgot Password ViewModel Completed

---
+++ app/src/main/java/com/halilkrkn/chatchef/presentation/LoginPage/viewModel/ForgotPasswordViewModel.kt
@@ -0,0 +1,45 @@
+package com.halilkrkn.chatchef.presentation.LoginPage.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+import com.halilkrkn.chatchef.data.firebase.FirebaseResult
+import com.halilkrkn.chatchef.data.firebase.implementation.AuthServiceImpl
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+import kotlinx.coroutines.launch
+
+data class AuthUiState(
+    val transactionState: Boolean = false,
+    val isLoading: Boolean = false,
+    val error: String? = null,
+)
+
+class ForgotPasswordViewModel: ViewModel() {
+
+    private val _uiState = MutableStateFlow(AuthUiState())
+    val uiState: StateFlow<AuthUiState> = _uiState
+    private val authRepository: AuthServiceImpl = AuthServiceImpl()
+
+    fun sendPasswordResetEmail(email: String) {
+        _uiState.value = _uiState.value.copy(isLoading = true)
+        viewModelScope.launch {
+            authRepository.sendPasswordResetEmail(email).collect{
+                result ->
+                when(result){
+                    is FirebaseResult.Loading -> {
+                        _uiState.value = _uiState.value.copy(isLoading = true)
+                    }
+                    is FirebaseResult.Success -> {
+                        _uiState.value = _uiState.value.copy(transactionState = true, isLoading = false)
+                    }
+                    is FirebaseResult.Error -> {
+                        _uiState.value = _uiState.value.copy(error = result.message, isLoading = false, transactionState = false)
+                    }
+                }
+            }
+        }
+    }
+
+}
