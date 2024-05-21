a25b0959939a22a2e5607b12560a1b947b4b0103
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-05-21 03:29:57 +0300

    UI Update & ViewModel Connected

--- app/src/main/java/com/halilkrkn/chatchef/presentation/LoginPage/LoginScreen.kt
+++ app/src/main/java/com/halilkrkn/chatchef/presentation/LoginPage/LoginScreen.kt
@@ -1,12 +1,19 @@
 package com.halilkrkn.chatchef.presentation.LoginPage
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,26 +33,43 @@ import com.halilkrkn.chatchef.presentation.components.UnderLinedTextComponent
 @Composable
 fun LoginScreen(navController: NavHostController, onLoginClick: () -> Unit = {}, onSignUpClick: () -> Unit = {}) {
 
+    var email by remember {mutableStateOf("")}
+    var password by remember {mutableStateOf("")}
+
     Scaffold(
         modifier = Modifier.padding(18.dp)){
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it)
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
 
-            Loader(R.raw.login_anim)
+            Loader(R.raw.login_anim, height = 250.dp)
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            TextFieldComponent("Email", painterResource(id = R.drawable.mail_icon))
-            PasswordFieldComponent("Password", painterResource(id = R.drawable.lock_icon))
+            TextFieldComponent(email, onValueChange = {
+                updatedEmail ->
+                email = updatedEmail
+            }, label = "Email", painterResource =  painterResource(id = R.drawable.mail_icon))
 
-            Spacer(modifier = Modifier.height(30.dp))
+            PasswordFieldComponent(password, label = "Password", onValueChange = {
+                    updatedPassword ->
+                password = updatedPassword
+            }, painterResource(id = R.drawable.lock_icon))
 
-            UnderLinedTextComponent(value = "Forgot your password?")
 
-            Spacer(modifier = Modifier.height(120.dp))
+
+
+            UnderLinedTextComponent(value = "Forgot your password?", onClick = {
+                navController.navigate(AuthScreen.ForgotPasswordScreen.route)
+            })
+
+
+
+            Spacer(modifier = Modifier.height(50.dp))
 
             ButtonComponent(value = "Login", onClick = {
                 onLoginClick()
